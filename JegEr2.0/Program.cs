d5bdf9da3268e3c89d84772ec4a3c36e1d2632e7
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-13 14:06:46 +0100

    Nå funker det faktisk. Skrevet navn i console blir tatt med

--- JegEr2.0/Program.cs
+++ JegEr2.0/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-såpofbjdmpåfnk
+Console.WriteLine("Hei, hva heter du?");
+var userNameInput = Console.ReadLine();
+Console.WriteLine($"Velkommen {userNameInput}!");

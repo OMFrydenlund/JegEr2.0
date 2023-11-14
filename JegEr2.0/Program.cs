4bb21c7fef0dcd4a2ec690ce3bbd87d57236bfbd
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-14 12:36:47 +0100

    Clean up

--- JegEr2.0/Program.cs
+++ JegEr2.0/Program.cs
@@ -1,12 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hei, hva heter du?");
+﻿Console.WriteLine("Hei, hva heter du?");
 var userNameInput = Console.ReadLine();
 Console.WriteLine($"Velkommen {userNameInput}!");
-
-int number = 3;
-long myLong = 700816;
-float myFloat = 79.9f;
-decimal myDecimal = 79.9M;
-double myDouble = 79.9;
-string myString = "Ja men halloheimornings";
-bool butIsItTrue = false;
